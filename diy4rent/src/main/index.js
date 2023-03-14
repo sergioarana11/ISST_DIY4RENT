43758cd8148d79d810b8ac60504e31729c817b64
sergioarana11 <116109517+sergioarana11@users.noreply.github.com>
2023-03-14 19:53:00 +0100

    Add files via upload

---
+++ diy4rent/src/main/index.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+    <header>
+
+
+<title>Diy4ent</title>
+//añadir archivo css
+
+ //<link rel="stylesheet" type="text/css" href="index.css">
+
+ </header>
+
+ <body>
+    <%body>
+
+
+ </body>
+</html>
