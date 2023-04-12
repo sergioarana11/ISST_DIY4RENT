e21957a19ee791afe14dff682c41232da3c121f8
SofiaPitaMiguelez <82121468+SofiaPitaMiguelez@users.noreply.github.com>
2023-04-12 20:10:31 +0200

    Add files via upload

---
+++ diy4rent/src/main/java/com/example/diy4rent/demo/repo/IUsuarioRepo.java
@@ -0,0 +1,11 @@
+package com.example.diy4rent.demo.repo;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.diy4rent.demo.model.Usuarios;
+
+public interface IUsuarioRepo extends JpaRepository<Usuarios, UUID> {
+        
+}
