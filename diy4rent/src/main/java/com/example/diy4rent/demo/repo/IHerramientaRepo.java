afaecc2e15ce44d9c3f22d92874949a9eeb2f27a
sergioarana11 <116109517+sergioarana11@users.noreply.github.com>
2023-05-02 18:39:33 +0200

    author maria galan

--- diy4rent/src/main/java/com/example/diy4rent/demo/repo/IHerramientaRepo.java
+++ diy4rent/src/main/java/com/example/diy4rent/demo/repo/IHerramientaRepo.java
@@ -1,9 +1,13 @@
 package com.example.diy4rent.demo.repo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.diy4rent.demo.model.Herramientas;
 
 public interface IHerramientaRepo extends JpaRepository<Herramientas, String> {
-        
+
+    List<Herramientas> findByTitleContaining(String title);
+
 }
