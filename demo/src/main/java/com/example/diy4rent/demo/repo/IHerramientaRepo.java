e31229b4e34c6d5946dfd6ea79b80b71b2928776
SofiaPitaMiguelez <82121468+SofiaPitaMiguelez@users.noreply.github.com>
2023-05-09 21:05:37 +0200

    Delete demo directory

--- demo/src/main/java/com/example/diy4rent/demo/repo/IHerramientaRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.diy4rent.demo.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.diy4rent.demo.model.Herramientas;
-
-public interface IHerramientaRepo extends JpaRepository<Herramientas, String> {
-        
-}
