499be19ec4a446c2502ce11884346fdad8f329ae
SofiaPitaMiguelez <82121468+SofiaPitaMiguelez@users.noreply.github.com>
2023-05-09 21:03:43 +0200

    Delete apih2/demo directory

--- apih2/demo/src/main/java/com/dyirent/marketplace/repository/HerramientaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.dyirent.marketplace.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.dyirent.marketplace.model.Herramienta;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface HerramientaRepository extends JpaRepository<Herramienta, Integer> {
-}
